b9d8cddecffb35d3099e6b751117dd216a1b4e92
stanley1228 <stanleyfreeze@gmail.com>
2017-11-05 12:31:02 +0800

    修改區段分法，分為直線差值，圓弧差值，固定
    修改區段分法，分為直線差值，圓弧差值，固定
    每個時間點欄位加入差值類型，是否要更新描繪架子

--- Test_7DOF_Sewing_Action_FB7roll.m
+++ Test_7DOF_Sewing_Action_FB7roll.m
@@ -11,6 +11,9 @@ DEF_TYPE_STOP=1;
 DEF_TYPE_LINE=2;
 DEF_TYPE_ARC=3;
 
+DEF_FRAME_UPDATE=1;
+DEF_FRAME_KEEP=2;
+
 L0=225;   %頭到肩膀
 L1=250;   %L型 長邊
 L2=25;    %L型 短邊
@@ -104,12 +107,12 @@ TranFrameToRobot=Needle_RobotF-Needle_ini_Plate;
 % end
 
 %右手圓周路徑
-ini_rotate_p_R=[-90 -90 0]+TranFrameToRobot;%從架子坐標系轉到手臂座標系
+ini_rotate_p_R=[-90 -90 0]+[140 0 0]+TranFrameToRobot;%往x方向前進140 從架子坐標系轉到手臂座標系
 rR=sqrt((ini_rotate_p_R(1)-Needle_RobotF(1))^2+(ini_rotate_p_R(2)-Needle_RobotF(2))^2);
 ini_rad_R=pi+atan((ini_rotate_p_R(2)-Needle_RobotF(2))/(ini_rotate_p_R(1)-Needle_RobotF(1)));%旋轉時的起始旋轉角度
 
 %左手圓周路徑
-ini_rotate_p_L=[90 90 0]+TranFrameToRobot;
+ini_rotate_p_L=[90 90 0]+[140 0 0]+TranFrameToRobot;%往x方向前進140
 rL=sqrt((ini_rotate_p_L(1)-Needle_RobotF(1))^2+(ini_rotate_p_L(2)-Needle_RobotF(2))^2);
 ini_rad_L=atan((ini_rotate_p_L(2)-Needle_RobotF(2))/(ini_rotate_p_L(1)-Needle_RobotF(1)));
 
@@ -191,7 +194,7 @@ L_p(S_R_BX_L_KEEP_1,:)=[210 -180 0 -90  90 0  90];
 %         210 -180 0 -90  90 0  90;
 %         210 -180 0 -90  90 0  90];
   
-%%==修改為不用分許多區段，只分為差值類型
+%%==修改為不用分許多區段，只分為差值類型，不考慮速度連續==%%
 index=2;
 for abst=0:DEF_CYCLE_TIME:TotalTime
  
@@ -201,8 +204,6 @@ for abst=0:DEF_CYCLE_TIME:TotalTime
     
     Itv=SeqItv(index);
     t=abst-Seqt(index-1);
-    
-  
     
     type=R_p_robotF(index,8);
     framupdate=R_p_robotF(index,9);
@@ -334,184 +335,6 @@ for abst=0:DEF_CYCLE_TIME:TotalTime
     Pcnt=Pcnt+1;      
     
 end
-
-
-
-%% ==產生縫紉流程路徑 不考慮速度連續== %%
-for abst=0:DEF_CYCLE_TIME:TotalTime
-    if abst<=Seqt(S_R_HOLD_L_HOLD_1)%右手夾 左手夾Seqt
-        Itv=SeqItv(S_R_HOLD_L_HOLD_1);
-        t=abst-Seqt(S_R_HOLD_L_HOLD_1-1);
-        
-        PathPlanPoint_R=R_p(S_R_HOLD_L_HOLD_1,:);
-        PathPlanPoint_L=L_p(S_R_HOLD_L_HOLD_1,:);
-        
-        %縫紉物四周抓取點座標
-        ObjCorner=[ PathPlanPoint_L(1:3)+HoldLen_L;
-                    PathPlanPoint_R(1:3)+HoldLen_R;
-                    PathPlanPoint_R(1:3);
-                    PathPlanPoint_L(1:3)];
-        
-    elseif abst<=Seqt(S_R_FX_L_FX) %右手往正X 140 左手往正X 140 
-        Itv=SeqItv(S_R_FX_L_FX);
-        t=abst-Seqt(S_R_FX_L_FX-1); %減上一筆的結束時間
-        
-        PathPlanPoint_R=R_p(S_R_FX_L_FX-1,:)+(R_p(S_R_FX_L_FX,:)-R_p(S_R_FX_L_FX-1,:))*t/Itv;%上一筆為起始點開始走
-        PathPlanPoint_L=L_p(S_R_FX_L_FX-1,:)+(L_p(S_R_FX_L_FX,:)-L_p(S_R_FX_L_FX-1,:))*t/Itv;
-        
-       
-        ObjCorner=[ PathPlanPoint_L(1:3)+HoldLen_L;
-                    PathPlanPoint_R(1:3)+HoldLen_R;
-                    PathPlanPoint_R(1:3);
-                    PathPlanPoint_L(1:3)];
-        
-  
-    elseif abst<=Seqt(S_R_KEEP_L_REL_1)%右手不動 左手開1
-        Itv=SeqItv(S_R_KEEP_L_REL_1);
-        t=abst-Seqt(S_R_KEEP_L_REL_1-1);
-        
-        PathPlanPoint_R=R_p(S_R_KEEP_L_REL_1,:);
-        PathPlanPoint_L=L_p(S_R_KEEP_L_REL_1,:);
-
-    elseif abst<=Seqt(S_R_KEEP_L_FX)%右手不動 左手往正X 180
-        Itv=SeqItv(S_R_KEEP_L_FX);
-        t=abst-Seqt(S_R_KEEP_L_FX-1);
-        
-        PathPlanPoint_R=R_p(S_R_KEEP_L_FX-1,:)+(R_p(S_R_KEEP_L_FX,:)-R_p(S_R_KEEP_L_FX-1,:))*t/Itv;
-        PathPlanPoint_L=L_p(S_R_KEEP_L_FX-1,:)+(L_p(S_R_KEEP_L_FX,:)-L_p(S_R_KEEP_L_FX-1,:))*t/Itv;
-
-    elseif abst<=Seqt(S_R_KEEP_L_HOLD_1)%右手不動 左手夾1   
-        Itv=SeqItv(S_R_KEEP_L_HOLD_1);
-        t=abst-Seqt(S_R_KEEP_L_HOLD_1-1);
-        
-        PathPlanPoint_R=R_p(S_R_KEEP_L_HOLD_1,:);
-        PathPlanPoint_L=L_p(S_R_KEEP_L_HOLD_1,:);
-    
-        
-     
-    elseif abst<=Seqt(S_R_FCIRX_L_BCIRX)%右手旋轉往正X 左手旋轉往負X
-        Itv=SeqItv(S_R_FCIRX_L_BCIRX);
-        t=abst-Seqt(S_R_FCIRX_L_BCIRX-1);
-        
-        PathPlanPoint_R=[Needle_RobotF 0 0 0 0] +rR*[cos(0.5*pi*t/Itv+ini_rad_R) sin(0.5*pi*t/Itv+ini_rad_R) 0 0 0 0 0]+[0 0 0 R_p(S_R_FCIRX_L_BCIRX-1,4:7)+(R_p(S_R_FCIRX_L_BCIRX,4:7)-R_p(S_R_FCIRX_L_BCIRX-1,4:7))*t/Itv]; %右手上到下弧形
-        PathPlanPoint_L=[Needle_RobotF 0 0 0 0] +rL*[cos(0.5*pi*t/Itv+ini_rad_L) sin(0.5*pi*t/Itv+ini_rad_L) 0 0 0 0 0]+[0 0 0 L_p(S_R_FCIRX_L_BCIRX-1,4:7)+(L_p(S_R_FCIRX_L_BCIRX,4:7)-L_p(S_R_FCIRX_L_BCIRX-1,4:7))*t/Itv]; %左手上到下弧形
-        
-     
-        ObjCenter=(PathPlanPoint_R(1:3)+PathPlanPoint_L(1:3))/2;%計算縫紉物四周抓取點座標
-        V_oc_lend=PathPlanPoint_L(1:3)-ObjCenter;%縫紉物中心點到左手的向量
-        V_oc_lend_ro_p90=[V_oc_lend 1]*Rz(0.5*pi);
-        V_oc_lend_ro_n90=[V_oc_lend 1]*Rz(-0.5*pi);
-        ObjCorner=[ PathPlanPoint_L(1:3); 
-                    ObjCenter+V_oc_lend_ro_p90(1:3);
-                    PathPlanPoint_R(1:3);
-                    ObjCenter+V_oc_lend_ro_n90(1:3)];
-                
-    elseif abst<=Seqt(S_R_REL_L_HOLD_1)%右手開 左手不動1
-        Itv=SeqItv(S_R_REL_L_HOLD_1);
-        t=abst-Seqt(S_R_REL_L_HOLD_1-1);
-        
-        PathPlanPoint_R=R_p(S_R_REL_L_HOLD_1,:);
-        PathPlanPoint_L=L_p(S_R_REL_L_HOLD_1,:);
-    
-    elseif abst<=Seqt(S_R_BX_L_KEEP_1)%右手往X負  左手不動1 
-        Itv=SeqItv(S_R_BX_L_KEEP_1);
-        t=abst-Seqt(S_R_BX_L_KEEP_1-1);
-
-        PathPlanPoint_R=R_p(S_R_BX_L_KEEP_1-1,:)+(R_p(S_R_BX_L_KEEP_1,:)-R_p(S_R_BX_L_KEEP_1-1,:))*t/Itv;
-        PathPlanPoint_L=L_p(S_R_BX_L_KEEP_1-1,:)+(L_p(S_R_BX_L_KEEP_1,:)-L_p(S_R_BX_L_KEEP_1-1,:))*t/Itv;
-    end
-    
-    in_x_end_R=PathPlanPoint_R(1);
-    in_y_end_R=PathPlanPoint_R(2);
-    in_z_end_R=PathPlanPoint_R(3);
-    
-    in_x_end_L=PathPlanPoint_L(1);
-    in_y_end_L=PathPlanPoint_L(2);
-    in_z_end_L=PathPlanPoint_L(3);
-
-    in_alpha_R=PathPlanPoint_R(4)*(pi/180);
-    in_beta_R=PathPlanPoint_R(5)*(pi/180);
-    in_gamma_R=PathPlanPoint_R(6)*(pi/180);
-    
-    in_alpha_L=PathPlanPoint_L(4)*(pi/180);
-    in_beta_L=PathPlanPoint_L(5)*(pi/180);
-    in_gamma_L=PathPlanPoint_L(6)*(pi/180);
-    
-    Rednt_alpha_R=PathPlanPoint_R(7)*(pi/180);
-    Rednt_alpha_L=PathPlanPoint_L(7)*(pi/180);
-    
-    %末點位置in==>IK==>theta==>FK==>末點位置out
-    %inverse kinematic
-    in_linkL=[L0;L1;L2;L3;L4;L5];
-    in_base=[0;-L0;0];%header0 座標系偏移到shoulder0 座標系 差Y方向的L0
-    in_end=[in_x_end_R;in_y_end_R;in_z_end_R];
-    in_PoseAngle=[in_alpha_R;in_beta_R;in_gamma_R];
-    %tic 
-    theta_R=IK_7DOF_FB7roll(DEF_RIGHT_HAND,in_linkL,in_base,in_end,in_PoseAngle,Rednt_alpha_R);
-    %toc
-    %AngleConstrain
-    bover=AngleOverConstrain(DEF_RIGHT_HAND,theta_R);
-    if bover == true
-        break;
-    end    
-    
-    in_linkL=[L0;L1;L2;L3;L4;L5];
-    in_base=[0;L0;0];
-    in_end=[in_x_end_L;in_y_end_L;in_z_end_L];
-    in_PoseAngle=[in_alpha_L;in_beta_L;in_gamma_L];
-    theta_L=IK_7DOF_FB7roll(DEF_LEFT_HAND,in_linkL,in_base,in_end,in_PoseAngle,Rednt_alpha_L);
-    
-    %AngleConstrain
-    bover=AngleOverConstrain(DEF_LEFT_HAND,theta_L);
-    if bover == true
-        break;
-    end    
-    
-    %forward kinematic
-    [out_x_end_R,out_y_end_R,out_z_end_R,out_alpha_R,out_beta_R,out_gamma_R,ArmJoint_R,RotationM_R] = FK_7DOF_FB7roll(DEF_RIGHT_HAND,L0,L1,L2,L3,L4,L5,x_base_R,y_base_R,z_base_R,theta_R);
-    [out_x_end_L,out_y_end_L,out_z_end_L,out_alpha_L,out_beta_L,out_gamma_L,ArmJoint_L,RotationM_L] = FK_7DOF_FB7roll(DEF_LEFT_HAND,L0,L1,L2,L3,L4,L5,x_base_L,y_base_L,z_base_L,theta_L);
-
-    %記錄每軸角度變化
-    PathTheta_R(Pcnt,1:7)=theta_R*(180/pi);
-    PathTheta_L(Pcnt,1:7)=theta_L*(180/pi);
-    
-    PathPlanPoint_R=[in_x_end_R in_y_end_R in_z_end_R in_alpha_R in_beta_R in_gamma_R Rednt_alpha_R];
-    PathIFKPoint_R=[out_x_end_R out_y_end_R out_z_end_R out_alpha_R out_beta_R out_gamma_R Rednt_alpha_R];%Rednt_alpha_R 還不會算，直接用跟規劃的依樣
-    
-    PathPlanPoint_L=[in_x_end_L in_y_end_L in_z_end_L in_alpha_L in_beta_L in_gamma_L Rednt_alpha_L];
-    PathIFKPoint_L=[out_x_end_L out_y_end_L out_z_end_L out_alpha_L out_beta_L out_gamma_L Rednt_alpha_L];
-    
-    %確認FK 和IK誤差
-%     if(out_x_end-in_x_end)>1e-5 || (out_y_end-in_y_end)>1e-5 || (out_z_end-in_z_end)>1e-5 || (out_alpha-in_alpha)>1e-5 || (out_beta-in_beta)>1e-5 || (out_gamma-in_gamma)>1e-5 
-%         display('===============')
-%         display('IK FK not match')
-%         i
-%         In=[in_x_end in_y_end in_z_end in_alpha*(180/pi) in_beta*(180/pi) in_gamma*(180/pi)]
-%         Out=[out_x_end out_y_end out_z_end out_alpha*(180/pi) out_beta*(180/pi) out_gamma*(180/pi)]
-%         
-%         break;
-%     end
-    
-   
-    %Path_R(Pcnt,1:7)=P_R;  %規畫的路徑點
-    %Path_L(Pcnt,1:7)=P_L;  %規畫的路徑點
-     
-    %記錄規劃路徑上的點
-    PathPlanPointRec_R(Pcnt,1:7)=PathPlanPoint_R;
-    PathPlanPointRec_L(Pcnt,1:7)=PathPlanPoint_L;
-    
-    %記錄經過IK FK運算後路徑上的點
-    PathIFKPointRec_R(Pcnt,1:7)=PathIFKPoint_R;
-    PathIFKPointRec_L(Pcnt,1:7)=PathIFKPoint_L;
-    
-    %畫關節點圖
-    %Draw_7DOF_FB7roll_point_dual(P_R,RotationM_R,PathPoint_R,PathPoint_L,RotationM_L,PathPoint_L);
-    Draw_7DOF_FB7roll_point_dual_script;
-    
-    pause(0.001);
-    Pcnt=Pcnt+1;      
-end
-
 
 %==畫在cartesian space下各自由度(x,y,z)的規劃
 %right hand
